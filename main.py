b1e2e5f9657b9dddff7ee9249bf8c0aa2eedabd7
VollKornTreiber <donbeppe@hotmail.de>
2023-03-07 22:05:16 +0100

    230307

--- main.py
+++ main.py
@@ -3,17 +3,35 @@
 import sys, pygame
 from player import Player
 
+#tweeks
+FULLSCREEN = 0
+
 class Game:
     def __init__(self):
         #Field setup
-        self.field_surf = pygame.Surface((screen_W * .6, screen_H))
+        self.field_surf = pygame.Surface((screen_W * .5, screen_H))
         self.field_surf.fill((20, 20, 20))
+        self.field_rect = self.field_surf.get_rect(topleft = (0, 0))
+        self.wall_thickness = 30
         
         #Player setup
-        self.player = Player((self.field_surf.get_width() / 2, self.field_surf.get_height() * .95), 5, self.field_surf.get_width())
+        player_sprite = Player((screen_W / 2, self.field_rect.height * .95), 5, [screen_W / 2 - self.field_surf.get_width() / 2 + (self.wall_thickness / 2), screen_W / 2 + self.field_surf.get_width() / 2 - (self.wall_thickness / 2)])
+        self.player = pygame.sprite.GroupSingle(player_sprite)
+
+    def draw_field(self):
+        screen.blit(self.field_surf, (screen_W / 2 - self.field_surf.get_width() / 2, 0))
+        pygame.draw.lines(self.field_surf, "darkgray", False, [self.field_rect.bottomleft, self.field_rect.topleft, self.field_rect.topright, self.field_rect.bottomright], self.wall_thickness)
+        pygame.draw.lines(self.field_surf, "lightgray", False, [self.field_rect.bottomleft, self.field_rect.topleft, self.field_rect.topright, self.field_rect.bottomright], int(self.wall_thickness * .85))
+        pygame.draw.lines(self.field_surf, "white", False, [self.field_rect.bottomleft, self.field_rect.topleft, self.field_rect.topright, self.field_rect.bottomright], int(self.wall_thickness * .6))
 
     def run(self):
-        screen.blit(self.field_surf, (screen_W / 2 - self.field_surf.get_width() / 2, 0))    #screen_W / 2 - self.field_surf.get_width() / 2
+        #logic update
+        self.player.update()
+
+        #image update
+        self.draw_field()
+        self.player.draw(screen)
+        
 
 
 if __name__ == "__main__":
@@ -25,8 +43,14 @@ if __name__ == "__main__":
     #Setup
     pygame.init()
     screen_W = 1200
-    screen_H = 900
-    screen = pygame.display.set_mode((screen_W, screen_H))
+    screen_H = 675
+
+    if FULLSCREEN: 
+        screen = pygame.display.set_mode((screen_W, screen_H), pygame.FULLSCREEN|pygame.SCALED)
+    else:
+        screen = pygame.display.set_mode((screen_W, screen_H))
+
+    pygame.display.set_caption("BreakOut Clone by VollKornTreiber")
     clock = pygame.time.Clock()
 
     #Game-Instance
@@ -40,6 +64,7 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
                 end()
+            
         
         screen.fill((50, 50, 50))
         game.run()
--- player.py
+++ player.py
@@ -1,11 +1,34 @@
 import pygame
 
-class Player:
-    def __init__(self, pos, spd, constraint):
+class Player(pygame.sprite.Sprite):
+    def __init__(self, start_pos, spd, x_constr):
         super().__init__()
-        self.image = pygame.surface.Surface((50, 10))
-        self.rect = self.image.get_rect(midbottom = pos)
-        self.pos = pos
+        self.image = pygame.surface.Surface((90, 15))
+        self.image.fill("white")
+        self.rect = self.image.get_rect(midbottom = start_pos)
         self.speed = spd
-        self.x_constraint = constraint
+        self.x_constr = x_constr
 
+    def move(self):
+        #input
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            if self.rect.left <= self.x_constr[0]:
+                self.rect.left = self.x_constr[0]
+            else:
+                if keys[pygame.K_LSHIFT]:
+                    self.rect.x -= self.speed + 5   #accelerate
+                else:
+                    self.rect.x -= self.speed   #regular speed
+
+        if keys[pygame.K_RIGHT]:
+            if self.rect.right >= self.x_constr[1]:
+                self.rect.right = self.x_constr[1]
+            else:
+                if keys[pygame.K_LSHIFT]:
+                    self.rect.x += self.speed + 5   #accelerate
+                else:
+                    self.rect.x += self.speed   #regular speed
+
+    def update(self):
+        self.move()
